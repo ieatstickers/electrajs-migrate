a204d880f7724d22ce66afd71917bdf27861d146
mikeburke <mikeburke.214@gmail.com>
2023-12-02 12:07:13 +0000

    - Added migrate file to run CLI application

---
+++ migrate.ts
@@ -0,0 +1,73 @@
+import application from "caporal";
+import { Container } from "./src/DI/Container";
+import { Commands } from "./src/Commands";
+import chalk from "chalk";
+import "reflect-metadata";
+
+function dbAction(
+  action: (
+    args: { [k: string]: any },
+    options: { [k: string]: any },
+    logger: Logger
+  ) => Promise<void>
+): ActionCallback
+{
+  return async (args, options, logger) => {
+    // Initialise MigrationDb
+    await Container.ensureMigrationDbExists();
+    await Container.getMigrationDb().initialize();
+    
+    // Run action
+    await action(args, options, logger);
+    
+    // Destroy MigrationDb
+    await Container.getMigrationDb().destroy();
+  };
+}
+
+(async () => {
+  
+  await Container.loadConfig();
+  
+  try
+  {
+    application
+      .name("Electra Migrate")
+      .description("MySQL Migrations for Node.js Applications")
+      .version(require('./package.json').version);
+    
+    // status
+    application
+      .command('status', 'Show the status of all migrations')
+      .action(
+        dbAction(async () => {
+          await Commands.status();
+        })
+      );
+    
+    // migrate
+    application
+      .command('migrate', 'Run all migrations')
+      .action(
+        dbAction(async () => {
+          await Commands.migrate();
+        })
+      );
+    
+    // rollback
+    application
+      .command('rollback', 'Rollback the last batch of migrations')
+      .action(
+        dbAction(async () => {
+          await Commands.rollback();
+        })
+      );
+    
+    application.parse(process.argv);
+  }
+  catch (e)
+  {
+    console.log(chalk.red(e.stack));
+  }
+  
+})();
