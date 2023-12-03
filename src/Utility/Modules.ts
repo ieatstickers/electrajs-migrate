3dd11c3755bc956f5dda6d14df6a7b8772cc8a1e
mikeburke <mikeburke.214@gmail.com>
2023-12-03 21:59:16 +0000

    Always use import() for dynamic modules

--- src/Utility/Modules.ts
+++ src/Utility/Modules.ts
@@ -1,19 +1,8 @@
-
-declare const __non_webpack_require__: typeof require;
-
 export class Modules
 {
   public static async import(path: string): Promise<any>
   {
-    if (import.meta?.url)
-    {
-      console.log(`await import(/* webpackIgnore: true */ '${path}')`);
-      return await import(/* webpackIgnore: true */ `${path}`);
-    }
-    else
-    {
-      console.log(`__non_webpack_require__('${path}')`);
-      return __non_webpack_require__(`${path}`);
-    }
+    console.log(`await import(/* webpackIgnore: true */ '${path}')`);
+    return await import(/* webpackIgnore: true */ `${path}`);
   }
 }
