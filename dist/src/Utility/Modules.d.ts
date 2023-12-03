9d39d7cdb5b7ad699726af17aa37fbad585fc0e5
mikeburke <mikeburke.214@gmail.com>
2023-12-03 22:16:45 +0000

    Added overload signature to Modules.import to allow exportName to be specified

--- dist/src/Utility/Modules.d.ts
+++ dist/src/Utility/Modules.d.ts
@@ -1,3 +1,4 @@
 export declare class Modules {
+    static import(exportName: string, path: string): Promise<any>;
     static import(path: string): Promise<any>;
 }
--- src/Utility/Modules.ts
+++ src/Utility/Modules.ts
@@ -1,8 +1,14 @@
 export class Modules
 {
+  public static async import(exportName: string, path: string): Promise<any>
   public static async import(path: string): Promise<any>
+  public static async import(...args: Array<any>): Promise<any>
   {
-    console.log(`await import(/* webpackIgnore: true */ '${path}')`);
-    return await import(/* webpackIgnore: true */ `${path}`);
+    const [ firstArg, secondArg ] = args;
+    const exportName = secondArg != null ? firstArg : 'default';
+    const path = secondArg || firstArg;
+    console.log(`Importing ${exportName} from ${path}`);
+    const module = await import(/* webpackIgnore: true */ `${path}`);
+    return module[exportName];
   }
 }
