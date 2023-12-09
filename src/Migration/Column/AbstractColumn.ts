25f6123039027e3137959a0610b7cd89c5cad05f
mikeburke <mikeburke.214@gmail.com>
2023-12-09 10:54:39 +0000

    - Added additional validation on column names

--- src/Migration/Column/AbstractColumn.ts
+++ src/Migration/Column/AbstractColumn.ts
@@ -8,7 +8,13 @@ export abstract class AbstractColumn implements ColumnInterface
   
   protected validateName(name: string): boolean
   {
-    const { valid, message } = Validators.string().validate(name);
+    const { valid, message } = Validators
+      .all([
+        Validators.string(),
+        Validators.minLength(1),
+        Validators.regex(/^[a-zA-Z_][a-zA-Z0-9_]{0,63}$/, 'A-z, 0-9 and/or _')
+      ])
+      .validate(name);
     if (!valid) throw new TypeError(`Invalid ${this.constructor.name} name: ${message}`);
     return true;
   }
