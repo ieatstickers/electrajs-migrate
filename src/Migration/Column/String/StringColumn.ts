8a818d37b8ea4a2ff835b02d556bc07dc29ae75b
mikeburke <mikeburke.214@gmail.com>
2023-12-09 11:04:27 +0000

    - Implemented abstract helper methods in string column

--- src/Migration/Column/String/StringColumn.ts
+++ src/Migration/Column/String/StringColumn.ts
@@ -59,30 +59,28 @@ export class StringColumn extends AbstractColumn implements ColumnInterface
       if (this.options.length !== undefined) columnDefinition += `(${this.options.length})`;
     }
     
-    // addBefore
-    if (this.options.addBefore)
-    {
-      columnDefinition += ` BEFORE ${await connection.escape(this.options.addBefore)}`;
-    }
-    // addAfter
-    else if (this.options.addAfter)
-    {
-      columnDefinition += ` AFTER ${await connection.escape(this.options.addAfter)}`;
-    }
+    // before / after
+    columnDefinition = this.addPositionStatement(
+      columnDefinition,
+      this.options.addBefore ? await connection.escape(this.options.addBefore) : undefined,
+      this.options.addAfter ? await connection.escape(this.options.addAfter) : undefined,
+      !createTable
+    );
     
     // nullable
-    this.options.nullable
-      ? columnDefinition += " NULL"
-      : columnDefinition += " NOT NULL";
+    columnDefinition = this.addNullableStatement(columnDefinition, this.options.nullable);
     
     // primaryKey
-    if (this.options.primaryKey) columnDefinition += " PRIMARY KEY";
+    columnDefinition = this.addPrimaryKeyStatement(columnDefinition, this.options.primaryKey);
     
     // default
-    if (this.options.default !== undefined) columnDefinition += ` DEFAULT ${await connection.escape(this.options.default)}`;
+    columnDefinition = this.addDefaultStatement(
+      columnDefinition,
+      this.options.default ? await connection.escape(this.options.default) : undefined
+    );
     
     // index
-    if (this.options.index) columnDefinition += " INDEX";
+    columnDefinition = this.addIndexStatement(columnDefinition, this.options.index);
     
     // Create new table
     if (createTable)
