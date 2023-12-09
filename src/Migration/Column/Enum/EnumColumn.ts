1088b3d984a815ad54bdbf1fcb7dea4a6275cc79
mikeburke <mikeburke.214@gmail.com>
2023-12-09 11:03:01 +0000

    - Implemented abstract helper methods in enum column

--- src/Migration/Column/Enum/EnumColumn.ts
+++ src/Migration/Column/Enum/EnumColumn.ts
@@ -60,27 +60,25 @@ export class EnumColumn extends AbstractColumn implements ColumnInterface
     
     let columnDefinition = `${escapedColumnName} ENUM('${escapedValues.join("', '")}')`;
     
-    // addBefore
-    if (this.options.addBefore)
-    {
-      columnDefinition += ` BEFORE ${await connection.escape(this.options.addBefore)}`;
-    }
-    // addAfter
-    else if (this.options.addAfter)
-    {
-      columnDefinition += ` AFTER ${await connection.escape(this.options.addAfter)}`;
-    }
+    // before / after
+    columnDefinition = this.addPositionStatement(
+      columnDefinition,
+      this.options.addBefore ? await connection.escape(this.options.addBefore) : undefined,
+      this.options.addAfter ? await connection.escape(this.options.addAfter) : undefined,
+      !createTable
+    );
     
     // nullable
-    this.options.nullable
-      ? columnDefinition += " NULL"
-      : columnDefinition += " NOT NULL";
+    columnDefinition = this.addNullableStatement(columnDefinition, this.options.nullable);
     
     // default
-    if (this.options.default !== undefined) columnDefinition += ` DEFAULT '${await connection.escape(this.options.default)}'`;
+    columnDefinition = this.addDefaultStatement(
+      columnDefinition,
+      this.options.default ? await connection.escape(this.options.default) : undefined
+    );
     
     // index
-    if (this.options.index) columnDefinition += " INDEX";
+    columnDefinition = this.addIndexStatement(columnDefinition, this.options.index);
     
     // Create new table
     if (createTable)
