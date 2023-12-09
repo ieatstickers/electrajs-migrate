5be82dc85c8d3b94483b7800ce0a47ef804371ef
mikeburke <mikeburke.214@gmail.com>
2023-12-09 10:58:07 +0000

    - Implemented abstract helper methods in blob column

--- src/Migration/Column/Blob/BlobColumn.ts
+++ src/Migration/Column/Blob/BlobColumn.ts
@@ -45,21 +45,16 @@ export class BlobColumn extends AbstractColumn implements ColumnInterface
     // type
     let columnDefinition = `${escapedColumnName} ${this.options.type}`;
     
-    // addBefore
-    if (this.options.addBefore)
-    {
-      columnDefinition += ` BEFORE ${await connection.escape(this.options.addBefore)}`;
-    }
-    // addAfter
-    else if (this.options.addAfter)
-    {
-      columnDefinition += ` AFTER ${await connection.escape(this.options.addAfter)}`;
-    }
+    // before / after
+    columnDefinition = this.addPositionStatement(
+      columnDefinition,
+      this.options.addBefore ? await connection.escape(this.options.addBefore) : undefined,
+      this.options.addAfter ? await connection.escape(this.options.addAfter) : undefined,
+      !createTable
+    );
     
     // nullable
-    this.options.nullable
-      ? columnDefinition += " NULL"
-      : columnDefinition += " NOT NULL";
+    columnDefinition = this.addNullableStatement(columnDefinition, this.options.nullable);
     
     // Create new table
     if (createTable)
