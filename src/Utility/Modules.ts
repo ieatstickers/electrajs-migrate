4305bb0faa704faffb019ecc7172c6fe611cb721
mikeburke <mikeburke.214@gmail.com>
2023-12-13 19:31:15 +0000

    - Testing require import for commonjs modules

--- src/Utility/Modules.ts
+++ src/Utility/Modules.ts
@@ -7,7 +7,8 @@ export class Modules
     const [ firstArg, secondArg ] = args;
     const exportName = secondArg != null ? firstArg : null;
     const path = secondArg != null ? secondArg : firstArg;
-    const module = await import(/* webpackIgnore: true */ `${path}`);
+    // const module = await import(/* webpackIgnore: true */ `${path}`);
+    const module = require(`${path}`);
     if (exportName) return module[exportName];
     return module;
   }
