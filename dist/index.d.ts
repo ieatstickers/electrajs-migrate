6bf297562a0feb555f5aa63a0a87503c713220e6
mikeburke <mikeburke.214@gmail.com>
2023-12-17 17:46:30 +0000

    - Exported column type enum classes

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,3 +1,6 @@
 export { AbstractMigration } from "./src/Migration/AbstractMigration";
 export { MigrationInterface } from "./src/Migration/MigrationInterface";
 export { MySql } from "./src/Migration/Database/MySql";
+export { BlobColumnTypeEnum } from "./src/Migration/Column/Blob/BlobColumnTypeEnum";
+export { IntColumnTypeEnum } from "./src/Migration/Column/Int/IntColumnTypeEnum";
+export { StringColumnTypeEnum } from "./src/Migration/Column/String/StringColumnTypeEnum";
