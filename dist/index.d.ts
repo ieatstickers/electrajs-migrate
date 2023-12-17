11c1e28cb3513d1fa32453a49ad432da473d8c03
mikeburke <mikeburke.214@gmail.com>
2023-12-17 15:20:33 +0000

    - Exported MySql class

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,2 +1,3 @@
 export { AbstractMigration } from "./src/Migration/AbstractMigration";
 export { MigrationInterface } from "./src/Migration/MigrationInterface";
+export { MySql } from "./src/Migration/Database/MySql";
--- index.ts
+++ index.ts
@@ -1,3 +1,4 @@
 
 export { AbstractMigration } from "./src/Migration/AbstractMigration";
 export { MigrationInterface } from "./src/Migration/MigrationInterface";
+export { MySql } from "./src/Migration/Database/MySql";
