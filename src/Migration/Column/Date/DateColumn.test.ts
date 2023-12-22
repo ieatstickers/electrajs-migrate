ad7737ad14b46a712dec6ea40741393cc1fafd1e
mikeburke <mikeburke.214@gmail.com>
2023-12-22 15:01:47 +0000

    - Refactored DateColumn options to chainable methods

--- src/Migration/Column/Date/DateColumn.test.ts
+++ src/Migration/Column/Date/DateColumn.test.ts
@@ -5,14 +5,69 @@ describe("DateColumn", () => {
   describe("constructor", () => {
     
     it("initializes properties correctly", () => {
-      const dateColumn = new DateColumn("dateOfBirth", { nullable: true, addAfter: "otherColumn" });
+      const dateColumn = new DateColumn("dateOfBirth");
       expect(dateColumn).toHaveProperty("name", "dateOfBirth");
-      expect(dateColumn).toHaveProperty("options", {
-        nullable: true,
-        default: undefined,
-        index: false,
-        addAfter: "otherColumn"
-      });
+      expect(dateColumn).toHaveProperty("options", {});
+    });
+    
+  });
+  
+  describe("nullable", () => {
+    
+    it("sets nullable option correctly", () => {
+      const dateColumn = new DateColumn("dateOfBirth");
+      dateColumn.nullable();
+      expect(dateColumn).toHaveProperty("options.nullable", true);
+    });
+    
+    it("throws error if invalid value is passed", () => {
+      const dateColumn = new DateColumn("dateOfBirth");
+      expect(() => dateColumn.nullable("invalid" as any)).toThrow(TypeError);
+    });
+    
+  });
+  
+  describe("default", () => {
+    
+    it("sets default option correctly", () => {
+      const dateColumn = new DateColumn("dateOfBirth");
+      dateColumn.default("2023-12-17");
+      expect(dateColumn).toHaveProperty("options.default", "2023-12-17");
+    });
+    
+    it("throws error if invalid value is passed", () => {
+      const dateColumn = new DateColumn("dateOfBirth");
+      expect(() => dateColumn.default(123 as any)).toThrow(TypeError);
+    });
+    
+  });
+  
+  describe("after", () => {
+    
+    it("sets after option correctly", () => {
+      const dateColumn = new DateColumn("dateOfBirth");
+      dateColumn.after("otherColumn");
+      expect(dateColumn).toHaveProperty("options.after", "otherColumn");
+    });
+    
+    it("throws error if invalid value is passed", () => {
+      const dateColumn = new DateColumn("dateOfBirth");
+      expect(() => dateColumn.after(123 as any)).toThrow(TypeError);
+    });
+    
+  });
+  
+  describe("index", () => {
+    
+    it("sets index option correctly", () => {
+      const dateColumn = new DateColumn("dateOfBirth");
+      dateColumn.index();
+      expect(dateColumn).toHaveProperty("options.index", true);
+    });
+    
+    it("throws error if invalid value is passed", () => {
+      const dateColumn = new DateColumn("dateOfBirth");
+      expect(() => dateColumn.index("invalid" as any)).toThrow(TypeError);
     });
     
   });
@@ -20,15 +75,27 @@ describe("DateColumn", () => {
   describe("getColumnDefinition", () => {
     
     it("returns correct column definition", async () => {
-      const dateColumn = new DateColumn("dateOfBirth", { nullable: true, addAfter: "otherColumn" });
+      const dateColumn = (new DateColumn("dateOfBirth"))
+        .nullable(true)
+        .after("otherColumn");
       const definition = dateColumn.getColumnDefinition().get();
       expect(definition).toEqual("`dateOfBirth` DATE NULL AFTER `otherColumn`");
     });
     
     it("returns correct column definition with default", async () => {
-      const dateColumn = new DateColumn("dateOfBirth", { nullable: true, addAfter: "otherColumn", default: "2023-12-17" });
+      const dateColumn = (new DateColumn("dateOfBirth"))
+        .nullable(true)
+        .after("otherColumn")
+        .default("2023-12-17");
       const definition = dateColumn.getColumnDefinition().get();
       expect(definition).toEqual("`dateOfBirth` DATE NULL DEFAULT '2023-12-17' AFTER `otherColumn`");
+    });
+    
+    it("returns correct column definition without nullable", async () => {
+      const dateColumn = (new DateColumn("dateOfBirth"))
+        .after("otherColumn");
+      const definition = dateColumn.getColumnDefinition().get();
+      expect(definition).toEqual("`dateOfBirth` DATE NOT NULL AFTER `otherColumn`");
     });
     
   });
@@ -36,16 +103,22 @@ describe("DateColumn", () => {
   describe("getIndexDefinition", () => {
     
     it("returns null when index option is false", async () => {
-      const dateColumn = new DateColumn("dateOfBirth", { nullable: true, addAfter: "otherColumn" });
+      const dateColumn = (new DateColumn("dateOfBirth"))
+        .nullable(true)
+        .after("otherColumn");
       const definition = dateColumn.getIndexDefinition();
       expect(definition).toBeNull();
     });
     
     it("returns correct index definition", async () => {
-      const dateColumn = new DateColumn("dateOfBirth", { nullable: true, addAfter: "otherColumn", index: true });
+      const dateColumn = (new DateColumn("dateOfBirth"))
+        .nullable(true)
+        .after("otherColumn")
+        .index(true);
       const definition = dateColumn.getIndexDefinition().get();
       expect(definition).toEqual("INDEX (`dateOfBirth`)");
     });
+    
     
   });
 });
--- src/Migration/Column/Date/DateColumn.ts
+++ src/Migration/Column/Date/DateColumn.ts
@@ -4,45 +4,53 @@ import { Validators } from "@electra/utility";
 import { AbstractColumn } from "../AbstractColumn";
 import { IndexDefinition } from "../../Definition/IndexDefinition";
 import { ColumnDefinition } from "../../Definition/ColumnDefinition";
+import { ColumnTypeEnum } from "../ColumnTypeEnum";
 
 export class DateColumn extends AbstractColumn implements ColumnInterface
 {
-  private readonly options: DateColumnOptions;
+  private readonly options: DateColumnOptions = {};
   
-  public constructor(name: string, options?: Partial<DateColumnOptions>)
+  public nullable(nullable: boolean = true): this
   {
-    super(name);
-    
-    this.options = {
-      nullable: false,
-      default: undefined,
-      index: false,
-      addAfter: undefined,
-      ...options
-    };
-    
-    this.validateOptions(
-      this.options,
-      {
-        nullable: Validators.boolean(),
-        default: Validators.regex(
-          /^\d{4}-(0[1-9]|1[0-2])-(0[1-9]|[12]\d|3[01])$/,
-          'YYYY-MM-DD',
-          { optional: true }
-        ),
-        index: Validators.boolean(),
-        addAfter: Validators.string({ optional: true })
-      }
-    );
+    const { valid, message } = Validators.boolean().validate(nullable);
+    if (valid === false) throw new TypeError(`Invalid value passed to DateColumn.nullable: ${message}`);
+    this.options.nullable = nullable;
+    return this;
+  }
+  
+  public default(value: string): this
+  {
+    const { valid, message } = Validators
+      .regex(/^\d{4}-(0[1-9]|1[0-2])-(0[1-9]|[12]\d|3[01])$/, "YYYY-MM-DD")
+      .validate(value);
+    if (valid === false) throw new TypeError(`Invalid value passed to DateColumn.default: ${message}`);
+    this.options.default = value;
+    return this;
+  }
+  
+  public index(index: boolean = true): this
+  {
+    const { valid, message } = Validators.boolean().validate(index);
+    if (valid === false) throw new TypeError(`Invalid value passed to DateColumn.index: ${message}`);
+    this.options.index = index;
+    return this;
+  }
+  
+  public after(columnName: string): this
+  {
+    const { valid, message } = this.validateColumnName(columnName);
+    if (valid === false) throw new TypeError(`Invalid value passed to DateColumn.after: ${message}`);
+    this.options.after = columnName;
+    return this;
   }
   
   public getColumnDefinition(): ColumnDefinition
   {
     return ColumnDefinition
-      .create(this.name, "DATE")
+      .create(this.name, ColumnTypeEnum.DATE)
       .nullable(this.options.nullable)
       .default(this.options.default ? `'${this.options.default}'` : undefined)
-      .after(this.options.addAfter);
+      .after(this.options.after);
   }
   
   public getIndexDefinition(): IndexDefinition
